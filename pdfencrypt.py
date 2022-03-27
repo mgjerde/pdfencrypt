e40ac8879b8c3af32dbcfbbf82a3eb81213d7d28
Marius Gjerde <marius@mgjerde.no>
2022-03-27 16:30:43 +0200

    first commit

---
+++ pdfencrypt.py
@@ -0,0 +1,58 @@
+from fileinput import filename
+import PyPDF3
+import os.path
+import sys
+import argparse
+
+def random():
+    return 42
+
+parser = argparse.ArgumentParser(description='Encrypt R Us', usage='%(prog)s filename [options]')
+parser.add_argument('pdffile', metavar='filename', type=argparse.FileType('rb'), help='The file you want to encrypt')
+# parser.add_argument('-i', '--interactive', action='store_true', help='Function to interactivly help with the inputs')
+parser.add_argument('-o', '--output', metavar='filename', help='Save the encrypted PDF file as this')
+parser.add_argument('--owner', metavar='passphrase', help='Set owner password')
+parser.add_argument('--user', metavar='passphrase', help='Set user password')
+
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+
+
+
+pdfReader = PyPDF3.PdfFileReader(args.pdffile)
+
+# args.pdffile.close()
+
+print('Argument List:', args )
+
+
+def encryptPDF(pdffil, userpwd=None, ownerpwd=None):
+    
+    pdfReader = PyPDF3.PdfFileReader(pdffil)
+   
+    pdfWriter = PyPDF3.PdfFileWriter()
+    for page in range(pdfReader.numPages):
+        pdfWriter.addPage(pdfReader.getPage(page))
+    pdfWriter.encrypt(user_pwd=userpwd,owner_pwd=ownerpwd)
+    resultpdf = open(f'encrypted.pdf', 'wb')
+    pdfWriter.write(resultpdf)
+    
+    resultpdf.close
+
+
+
+# import PyPDF2
+# pdfFile = open('input.pdf', 'rb')
+# # Create reader and writer object
+# pdfReader = PyPDF2.PdfFileReader(pdfFile)
+# pdfWriter = PyPDF2.PdfFileWriter()
+# # Add all pages to writer (accepted answer results into blank pages)
+# for pageNum in range(pdfReader.numPages):
+#     pdfWriter.addPage(pdfReader.getPage(pageNum))
+# # Encrypt with your password
+# pdfWriter.encrypt('password')
+# # Write it to an output file. (you can delete unencrypted version now)
+# resultPdf = open('encrypted_output.pdf', 'wb')
+# pdfWriter.write(resultPdf)
+# resultPdf.close()
+# Create the root window
